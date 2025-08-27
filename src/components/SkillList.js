e61977d58a72ef211e9fb4aff0769d4b7b692db8
Siddiha <fathimasiddika62@gmail.com>
2025-08-27 17:12:44 +0530

    xdd

--- src/components/SkillList.js
+++ src/components/SkillList.js
@@ -6,7 +6,7 @@ import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
 import FadeInSection from "./FadeInSection";
-import { platform } from "os";
+
 
 const isHorizontal = window.innerWidth < 600;
 
